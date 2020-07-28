5942c074e45a10f275c9c0ec280e3589ddbb246f
Grekus14 <georgepopovich14@gmail.com>
2020-07-28 11:37:01 +0200

    Fix minor warning

--- frontend/src/components/partials/ChatBox.jsx
+++ frontend/src/components/partials/ChatBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import io from "socket.io-client";
 import axiosApp from "../../utils/axiosConfig";
