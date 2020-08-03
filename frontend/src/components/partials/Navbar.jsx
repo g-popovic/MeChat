dc7de5d28a832f9ed9fb82ff065a885e60e68d00
Grekus14 <georgepopovich14@gmail.com>
2020-08-03 14:03:57 +0200

    Fix bug where page reloads upon New Post opening

--- frontend/src/components/partials/Navbar.jsx
+++ frontend/src/components/partials/Navbar.jsx
@@ -50,7 +50,7 @@ function Navbar(props) {
 						</Link>
 					</li>
 					<li className="nav-new-post">
-						<a onClick={toggleNewPost} href="">
+						<a onClick={toggleNewPost}>
 							<img
 								src={require("../../images/assets/Mobile Bottom Nav/Add Post.svg")}
 								alt="new post button"
